2e3a6b1011daf4b61bc9481bda0ca3b4c4a76570
Agustín García <agustinricardogarcia@gmail.com>
2024-10-15 15:49:09 -0300

    Añado componentes de alumno al backend

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const apunteRoutes = require('./routes/apunteRoutes');
 const precioRoutes = require('./routes/precioRoutes');
 const compraRoutes = require('./routes/compraRoutes');
+const alumnoRoutes = require('./routes/alumnoRoutes');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/api/apuntes', apunteRoutes);
 app.use('/api/precios', precioRoutes);
 app.use('/api/compras', compraRoutes);
+app.use('/api/alumnos', alumnoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
